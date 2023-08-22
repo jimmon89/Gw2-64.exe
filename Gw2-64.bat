6f9b88e356248e16286667f0f9b98cb24ccbca96
jimmon89 <james.e.adams89@gmail.com>
2023-08-22 13:42:49 -0400

    attempt to auto create release.

--- Gw2-64.bat
+++ Gw2-64.bat
@@ -2,6 +2,7 @@
 CD "%~dp0"
 ECHO %CD%
 START "" ".\Gw2.exe" %*
-PAUSE
+ECHO Press any key to launch Blish HUD...
+PAUSE >> NUL
 CD ".\BlishHUD"
 START "" ".\Blish HUD.exe"
--- Gw2-64.py
+++ Gw2-64.py
@@ -3,7 +3,5 @@ import os
 import sys
 BatchFileName = (os.path.splitext(sys.argv[0])[0]) + ".bat";
 BatchFileArguments = ' '.join(sys.argv[1:]);
-print([BatchFileName])
-print([BatchFileArguments])
 
 subprocess.run([(BatchFileName), (BatchFileArguments)]);
